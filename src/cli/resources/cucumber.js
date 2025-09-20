81b2c42f9340673279c547b191aa063462550864
iamkenos <alexis.matunog@gmail.com>
2025-09-21 09:10:46 +1000

    refactor: move init resources location

--- src/cli/resources/cucumber.js
+++
@@ -1,7 +0,0 @@
-const { configure } = require('@iamkenos/kyoko/config');
-
-// you can set most of the cucumber config props from here, leave some that are restricted.
-exports.default = configure({
-  baseURL: 'https://the-internet.herokuapp.com/',
-  parallel: 5
-});
